9437371c8408de53d44eccad75b2e29a56e81de2
sky <upebb@foxmail.com>
2017-12-24 21:19:43 +0800

    新增cookie js

--- src/utils/global.js
+++ src/utils/global.js
@@ -2,7 +2,8 @@ import 'media-match';
 import $ from 'jquery';
 import moment from 'moment';
 import numeral from 'numeral';
+import cookie from 'js-cookie';
 import PubSub from 'pubsub-js';
 import { Signal } from 'signals';
 
-Object.assign(window, { $, moment, numeral, PubSub, Signal });
+Object.assign(window, { $, moment, numeral, cookie, PubSub, Signal });
